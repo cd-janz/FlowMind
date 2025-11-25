792eda9fc7dc06c999b9816352af048885b02a10
cd-janz <cd.juanzuniga@gmail.com>
2025-11-24 22:40:13 -0500

    docker compose updated to add backend project, database tables has changed, backend and frontend reference commit updated

--- postgres/seeds.sql
+++ postgres/seeds.sql
@@ -1,2 +1,2 @@
-INSERT INTO ROLES(name, code) VALUES ('administrator', 'AD01');
-INSERT INTO ROLES(name, code) VALUES ('client', 'CL02');
+INSERT INTO ROLES(name, code, value) VALUES ('administrator', 'AD01', 7);
+INSERT INTO ROLES(name, code, value) VALUES ('client', 'CL01', 1);
